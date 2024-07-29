ca878e15f98add40274099b6f1f1e4386f44b93e
ozz1webdev <ozz1webdev@gmail.com>
2024-07-29 18:50:46 +0200

    add serializer urls and view

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -147,3 +147,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWS_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
