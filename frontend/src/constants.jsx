11eb7a0ec5c741fbc79b9046623a0d308b999d79
ozz1webdev <ozz1webdev@gmail.com>
2024-07-29 22:04:54 +0200

    add axios configs

---
+++ frontend/src/constants.jsx
@@ -0,0 +1,6 @@
+export const ACCESS_TOKEN = 'access_token';
+export const REFRESH_TOKEN = 'refresh_token';
+export const USER_LOGGED_IN = 'user_logged_in';
+export const USER_LOGGED_OUT = 'user_logged_out';
+export const USER = 'user';
+export const USER_INFO = 'user_info';
